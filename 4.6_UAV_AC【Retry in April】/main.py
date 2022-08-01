cb1ab6b0a4f20bf099c9010686667f27511c8a54
Zhenzhen Gong <zhenzhengong@Zhenzhens-MacBook-Pro.local>
2022-08-01 09:50:05 +0800

    4.6.6 force added as comparison

--- 4.6_UAV_AC【Retry in April】/main.py
+++ 4.6_UAV_AC【Retry in April】/main.py
@@ -7,6 +7,7 @@ import copy
 import matplotlib.pyplot as plt
 # import matplotlib
 from statistics import mean
+import pickle
 
 import torch
 import torch.nn as nn
@@ -21,23 +22,22 @@ from IoTEnv import Uav, Device, Env, Policy
 # Press ⌘F8 to toggle the breakpoint.
 
 
-parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor (default: 0.99)')
-parser.add_argument('--learning_rate', type=float, default=3e-1, metavar='lr',
-                    help='discount factor (default: 3e-1)')
-parser.add_argument('--seed', type=int, default=0, metavar='N',
-                    help='random seed (default: 0)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=1, metavar='N',
-                    help='interval between training status logs (default: 10)')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
+# parser.add_argument('--gamma', type=float, default=0.6, metavar='G',
+#                     help='discount factor (default: 0.99)')
+# parser.add_argument('--learning_rate', type=float, default=0.4, metavar='lr',
+#                     help='discount factor (default: 3e-1)')
+# parser.add_argument('--seed', type=int, default=0, metavar='N',
+#                     help='random seed (default: 0)')
+# parser.add_argument('--render', action='store_true',
+#                     help='render the environment')
+# parser.add_argument('--log-interval', type=int, default=1, metavar='N',
+#                     help='interval between training status logs (default: 10)')
+# args = parser.parse_args()
 
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
-np.random.seed(args.seed)
-torch.manual_seed(args.seed)
+
 
 
 
@@ -45,7 +45,12 @@ torch.manual_seed(args.seed)
 #  V: 72 km/h =  20 m/s
 #  field: 1 km * 1km
 #  dist:
-param = {'episodes': 2,'nTimeUnits': 2, 'num_Devices': 5, 'V': 72, 'field': 1, 'dist': 0.040, 'freq_low': 8, 'freq_high': 16}
+param = {'episodes': 60, 'nTimeUnits': 600, 'nTimeUnits_random': 600, 'nTimeUnits_force': 600,
+         'gamma': 0.99, 'learning_rate': 0.7, 'log_interval': 1, 'seed': 0,
+         'num_Devices': 9, 'V': 72, 'field': 1, 'dist': 0.040, 'freq_low': 8, 'freq_high': 16}
+np.random.seed(param['seed'])
+torch.manual_seed(param['seed'])
+
 Devices = []
 # for i in range(param['num_Devices']):
 #     Devices.append(Device(random.randint(param['freq_low'], param['freq_high']), random.randint(30, 70), param['field']))
@@ -54,15 +59,17 @@ Devices = []
 # Devices.append(Device(35, 50, param['field']))
 # Devices.append(Device(35, 60, param['field']))
 # Devices.append(Device(35, 70, param['field']))
-# Devices.append(Device(50, random.randint(30, 70), param['field']))
-# Devices.append(Device(40, random.randint(30, 70), param['field']))
-# Devices.append(Device(30, random.randint(30, 70), param['field']))
-Devices.append(Device(20, random.randint(30, 70), param['field']))
-Devices.append(Device(12, random.randint(30, 70), param['field']))
-# Devices.append(Device(9, random.randint(30, 70), param['field']))
+Devices.append(Device(50, random.randint(30, 70), param['field']))
+Devices.append(Device(45, random.randint(30, 70), param['field']))
 Devices.append(Device(40, random.randint(30, 70), param['field']))
-Devices.append(Device(5, random.randint(30, 70), param['field']))
-Devices.append(Device(4, random.randint(30, 70), param['field']))
+Devices.append(Device(35, random.randint(30, 70), param['field']))
+Devices.append(Device(30, random.randint(30, 70), param['field']))
+Devices.append(Device(25, random.randint(30, 70), param['field']))
+Devices.append(Device(20, random.randint(30, 70), param['field']))
+Devices.append(Device(15, random.randint(30, 70), param['field']))
+Devices.append(Device(10, random.randint(30, 70), param['field']))
+# Devices.append(Device(5, random.randint(30, 70), param['field']))
+# Devices.append(Device(4, random.randint(30, 70), param['field']))
 # Devices.append(Device(2, random.randint(30, 70), param['field']))
 
 
@@ -71,10 +78,14 @@ env = Env(Devices, UAV, param['nTimeUnits'])
 
 Devices_random = copy.deepcopy(Devices)
 UAV_random = copy.deepcopy(UAV)
-env_random = Env(Devices_random, UAV_random, param['nTimeUnits'])
+env_random = Env(Devices_random, UAV_random, param['nTimeUnits_random'])
+
+Devices_force = copy.deepcopy(Devices)
+UAV_force = copy.deepcopy(UAV)
+env_force = Env(Devices_force, UAV_force, param['nTimeUnits_force'])
 
 model = Policy(1 * param['num_Devices'] + 2, param['num_Devices'])
-optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)  # lr=3e-2
+optimizer = optim.Adam(model.parameters(), lr=param['learning_rate'])  # lr=3e-2
 eps = np.finfo(np.float32).eps.item()
 
 
@@ -102,6 +113,7 @@ def select_action(state):
     # the action to take
     return action.item()
 
+
 def finish_episode():
     """
     Training code. Calculates actor and critic loss and performs backprop.
@@ -115,7 +127,7 @@ def finish_episode():
     # calculate the true value using rewards returned from the environment
     for r in model.rewards[::-1]:
         # calculate the discounted value
-        R = r + args.gamma * R
+        R = r + param['gamma'] * R
         returns.insert(0, R)
 
     returns = torch.tensor(returns)
@@ -143,6 +155,7 @@ def finish_episode():
     # reset rewards and action buffer
     del model.rewards[:]
     del model.saved_actions[:]
+
 
 def learning():
 
@@ -175,9 +188,18 @@ def learning():
             # random action
             # action = np.random.randint(param['num_Devices'])
 
+            # compute the distance
+            # CPoint = self.UAV.location  # current location
+            # NPoint = self.Devices[action].location  # next location
+            # distance = np.linalg.norm(CPoint - NPoint)  # Compute the distance of two points
+            # Fly_time = 1 if distance == 0 else math.ceil(distance / self.UAV.V)
+            # t = t + Fly_time
+            # t = t + 1
+
+
             # take the action
             # state, reward, reward_Regular, t = env.step(state, action, t)
-            t = t + 1
+            t = t + 3
             state, reward_, reward_rest, reward = env.step(state, action, t)
             print(reward_)
             print(reward_rest)
@@ -187,18 +209,7 @@ def learning():
             rate1[-1].append(reward_ / reward)
             rate2[-1].append(reward_rest / reward)
 
-            # 强制action
-            # 强制选择action
-            # action_table = np.zeros(param['num_Devices'])  # 筛选出当前有新任务的设备
-            # for i in range(param['num_Devices']):
-            #     if Devices[i].TimeList[t] == 1:
-            #         action_table[i] = 1
-            # inx = np.where(action_table == 1)[0]
-            # # action = inx[np.random.randint(len(inx))] if inx else np.random.randint(param['num_Devices']) # 随机选一个去访问
-            # if inx.any():
-            #     action = inx[0]  # inx[np.random.randint(len(inx))] #优先选择变化最不频繁的
-            # else:
-            #     action = np.random.randint(param['num_Devices'])
+
 
 
 
@@ -271,7 +282,7 @@ def learning():
         # print('  ')
 
 
-        if i_episode % args.log_interval == 0:
+        if i_episode % param['log_interval'] == 0:
             print('Forced: Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
                   i_episode, ep_reward, ave_Reward))
         # print("**********************************************************************************")
@@ -319,29 +330,70 @@ def main():
 
 
     # †††††††††††††††††††††††††††††††††††††††Random Trajectory††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
-    print("Random teajectory: One Episode Only")
+    print("Random trajectory: One Episode Only")
     env_random.initialization(Devices_random, UAV_random)
     state_random = env_random.reset(Devices_random, UAV_random)
     ep_reward_random = 0
     t = 0
     n = 0  # logging fly behaviors
-    while t < param['nTimeUnits']:
-        t = t + 1
+    while t < param['nTimeUnits_random']:
+        t = t + 3
         n = n + 1
         action_random = np.random.randint(param['num_Devices'])  # 纯粹随机选择
         state_random, reward_, reward_rest, reward_random = env_random.step(state_random, action_random, t)
-        model.rewards_random.append(reward_random)
+        # model.rewards_random.append(reward_random)
         ep_reward_random += reward_random
-        model.actions_random.append(action_random)
-        model.states_random.append(state_random)
+        # model.actions_random.append(action_random)
+        # model.states_random.append(state_random)
         print("Random: The {} episode" " and the {} fly" " at the end of {} time slots. " "Visit device {}".format(1, n,t,action_random))
-    ave_Reward_random = ep_reward_random / param['nTimeUnits']
+    ave_Reward_random = ep_reward_random / param['nTimeUnits_random']
     print('Random: Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(1, ep_reward_random, ave_Reward_random))
     # †††††††††††††††††††††††††††††††††††††††Random Trajectory††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
 
-    learning()
 
-    # # Plotting Phase
+
+    # †††††††††††††††††††††††††††††††††††††††Forced Trajectory††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
+    print("Forced trajectory: One Episode Only")
+    env_force.initialization(Devices_force, UAV_force)
+    state_force = env_force.reset(Devices_force, UAV_force)
+    ep_reward_force = 0
+    t = 0
+    while t < param['nTimeUnits_random']:
+        t = t + 3
+        # 强制选择action
+        action_table_force = np.zeros(param['num_Devices'])  # 筛选出当前有新任务的设备
+        for i in range(param['num_Devices']):
+            if Devices_force[i].TimeList[t-1] == 1:
+                action_table_force[i] = 1
+        inx = np.where(action_table_force == 1)[0]
+        # action = inx[np.random.randint(len(inx))] if inx else np.random.randint(param['num_Devices']) # 随机选一个去访问
+        if inx.any():
+            action_force = inx[0]  # inx[np.random.randint(len(inx))] #优先选择变化最不频繁的
+        else:
+            action_force = np.random.randint(param['num_Devices'])
+        state_force, reward_, reward_rest, reward_force = env_force.step(state_force, action_force, t)
+        ep_reward_force += reward_force
+        print("Force: The {} episode" " and the {} fly" " at the end of {} time slots. " "Visit device {}".format(1, n,t,action_force))
+    ave_Reward_force = ep_reward_force / param['nTimeUnits_force']
+    print('Force: Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(1, ep_reward_force, ave_Reward_force))
+    # †††††††††††††††††††††††††††††††††††††††Forced Trajectory††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
+
+
+
+
+    # †††††††††††††††††††††††††††††††††††††††Smart Trajectory††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
+    learning()
+    # †††††††††††††††††††††††††††††††††††††††Smart Trajectory††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
+
+
+    avg = {}
+    avg['Ave_Reward'] = Ave_Reward
+    avg['ave_Reward_random'] = ave_Reward_random
+    avg['ave_Reward_force'] = ave_Reward_force
+    with open('fig_temp.pkl', 'wb') as f:
+        pickle.dump([model, env, param, avg, logging_timeline], f)
+
+    # †††††††††††††††††††††††††††††††††††††††Plotting Phase††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
     fig, ax = plt.subplots(1)
     # ax[0].plot(np.arange(i_episode), Ep_reward, label='Actor-Critic')
     # ax[0].set_xlabel('Episodes')  # Add an x-label to the axes.
@@ -349,12 +401,13 @@ def main():
     # ax[0].set_title("The ep_reward")  # Add a title to the axes.
 
     # ax.plot(np.arange(1, EP+1), Ave_Reward, label='%.0f  Devices, %.0f TimeUnits, %.0f  episodes' %(param['num_Devices'], param['nTimeUnits'], EP, args.gamma, args.learning_rate))
-    ax.plot(np.arange(1, EP+1), Ave_Reward, label= str(param['num_Devices']) + ' Devices,' + str(EP) + ' episodes,' +  str(param['nTimeUnits']) + ' TimeUnits,' +  str(args.gamma) + ' gamma,' + str(args.learning_rate) + ' lr')
+    ax.plot(np.arange(1, EP+1), Ave_Reward, label= str(param['num_Devices']) + ' Devices,' + str(EP) + ' episodes,' +  str(param['nTimeUnits']) + ' TimeUnits,' +  str(param['gamma']) + ' gamma,' + str(param['learning_rate']) + ' lr')
     ax.set_xlabel('Episodes')  # Add an x-label to the axes.
     ax.set_ylabel('Ave_Reward')  # Add a y-label to the axes.
-    ax.set_title("The Ave_Reward")  # Add a title to the axes.
+    ax.set_title("The Ave_Reward, NN:" + str(model.pattern))  # Add a title to the axes.
     ax.axhline(y = max(Ave_Reward), color='r', linestyle='--', linewidth='0.9',   label='Smart: ' + str(max(Ave_Reward)))
     ax.axhline(y = ave_Reward_random, color='b', linestyle='--', linewidth='0.9', label='Random:'+ str(ave_Reward_random))
+    ax.axhline(y = ave_Reward_force, color='g', linestyle='--', linewidth='0.9', label='Forced:'+ str(ave_Reward_force))
     ax.legend(loc="best")
 
 
