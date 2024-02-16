b00c1f284e2388578db6d8d21d2b75e456495140
Zhenzhen Gong <zhenzhengong@Zhenzhens-MacBook-Pro.local>
2024-02-16 15:28:40 +0000

    [V6.3] Tasks_temp.pkl, Tasks[0][0-40]:easy, Tasks[1][0-36]:difficult

--- Accessories/produce_tasks.py
+++ Accessories/produce_tasks.py
@@ -12,10 +12,11 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import pandas as pd
+from scipy.spatial.distance import pdist
 
-
-from associations import pg_rl
+from associations import pg_rl_values_policies_gradients
 from env import Task
+
 
 with open('../mu_sig.pkl', 'rb') as f:
     mu, sig = pickle.load(f)  # Load the mu and sig to extract normalized feature
@@ -24,7 +25,7 @@ with open('../TaDeLL_result_k_2.pkl', 'rb') as f:
     means_pg, means_tadell, niter, TaDeLL_Model, tasks0, tasks, testing_tasks, testing_tasks_pg, testing_tasks_TaDeLL = pickle.load(f)
 
 origin = np.array([[0], [0], [0], [0], [0]])
-
+origin_cos = np.array([[1], [1], [1], [1], [1]])
 
 def draw_curves(values_pg, values_TaDeLL):
     fig, ax = plt.subplots(1)
@@ -36,7 +37,7 @@ def draw_curves(values_pg, values_TaDeLL):
     ax.legend(loc = "best")
 
 # 0. Initialize tasks set
-Tasks = []
+# Tasks = []
 
 
 # 1. Create task
@@ -49,26 +50,49 @@ task.extract_feature(mu, sig)  # Normalize the plain_feature using mu, sig. self
 
 # 2. Evaluate pg_rl and TaDeLL
 tsk0 = copy.deepcopy(task)
-values_pg = pg_rl(tsk0, niter=50, lr=0.08)
+values_pg, policies_pg, gradients_pg = pg_rl_values_policies_gradients(tsk0, niter=50, lr=0.08)
 
 tsk1 = copy.deepcopy(task)
 TaDeLL_Model.getDictPolicy_Single(tsk1)  # task.policy['theta'] = np.array(theta_hat)
 policy_TaDeLL = tsk1.policy
-values_TaDeLL = pg_rl(tsk1, niter=50, lr=0.02)
+values_TaDeLL, policies_TaDeLL, gradients_TaDeLL = pg_rl_values_policies_gradients(tsk1, niter=50, lr=0.02)
 
 draw_curves(values_pg, values_TaDeLL)
 
+# 重要的原始属性
+# task.init_policy
+# task.plain_feature
+# task.feature
+# 记录pg-rl和TaDeLL的values，policies，gradients的记录
 task.values_pg = values_pg
 task.values_TaDeLL = values_TaDeLL
-task.policy_TaDeLL = policy_TaDeLL
+task.policy_pg =policies_pg
+task.policy_TaDeLL = policies_TaDeLL
+task.gradients_pg = gradients_pg
+task.gradients_TaDeLL = gradients_TaDeLL
+# 一些人工特征提取
+# task.cost_gap = values_TaDeLL[0] - values_pg[0]
+# task.first_gradient =
+task.distance = np.linalg.norm(task.feature - origin)
+task.cos_distance = 1 - np.dot(np.squeeze(task.feature), np.squeeze(origin_cos)) / (np.linalg.norm(np.squeeze(task.feature)) * np.linalg.norm(np.squeeze(origin_cos)))
+# 下面的计算方法可以得到一样的结果
+# task.cos_distance = pdist(np.vstack([np.squeeze(task.feature), np.squeeze(origin_cos)]), 'cosine')
 
-Tasks.append(task)
 
 
 # 3. Save task into set and pkl file
+# check the task and save it when it's good
+task.convergence_steps = 1 # 手动输入
+
+with open('Tasks_temp.pkl', 'rb') as f:
+    Tasks = pickle.load(f)
+
+Tasks[0].append(task)
+Tasks[1].append(task)
+
 
 with open('Tasks_temp.pkl', 'wb') as f:
-    pickle.dump([Tasks], f)
+    pickle.dump(Tasks, f)
 
 
 d = 1
--- associations.py
+++ associations.py
@@ -29,3 +29,44 @@ def pg_rl(task, niter=50, lr=0.08):
 
     return values_array
 
+
+
+
+def pg_rl_values_policies_gradients(task, niter=50, lr=0.08):
+    """
+    Basic policy gradient learning can work fine for env.Task
+    """
+    # niter = 50
+    values = []
+    policies = []
+    gradients = []
+
+    values.append(task.get_value(task.policy["theta"]))
+    policies.append(task.policy["theta"])
+
+    for i in range(niter):  # Iterate policy gradient process
+        path = task.collect_path(task.policy["theta"])
+
+        djd_theta = task.djd_nac(path)
+        task.policy["theta"] = task.policy["theta"] + lr * djd_theta
+
+        values.append(task.get_value(task.policy["theta"]))
+        policies.append(task.policy["theta"])
+        gradients.append(djd_theta)
+
+        # print(i)
+        # print("djd_theta", djd_theta, "policy:", task.policy["theta"], "rewards:", values[-1])
+    values_array = np.array(values)
+    policies_array = np.array(policies)
+    gradients_array = np.array(gradients)
+
+    # print("rewards_array:", values_array)
+    # # Plotting procedure
+    # plt.ion()
+    # fig, ax = plt.subplots()
+    # ax.plot(np.arange(niter) + 1, values_array)
+    # fig.show()
+    # plt.ioff()
+    # print("Hello")
+
+    return values_array, policies_array, gradients_array
